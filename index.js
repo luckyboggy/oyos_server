7e4b8c65cd2e2dbde2f0440fd44d1e694bbc5ca4
unknown <luckyboggy@gmail.com>
2024-04-07 22:25:05 +0500

    deploy_0704_03

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ const startApp = async () => {
     /* app.listen(PORT, IP_ADDRESS, () =>
       console.log(`Server has been started on port ${PORT}`)
     ); */
-    app.listen(433, IP_ADDRESS, () =>
+    app.listen(433, "srv.olgayudina.ru", () =>
       console.log(`Server has been started on port ${PORT}`)
     );
   } catch (err) {
